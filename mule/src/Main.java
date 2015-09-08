e30c8e025d402f138fc7895c6a73493fd6cfacce
Travis Clement <travisclement@yahoo.com>
2015-09-08 15:12:03 -0400

    initial commit

---
+++ mule/src/Main.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static Game mulegame;
+
+    public Main() {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartUP.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("M.U.L.E Main Screen");
+        primaryStage.setScene(new Scene(root));
+        StartUpController controller = fxmlLoader.getController();
+        controller.setMainApp(this);
+        primaryStage.show();
+    }
+
+    public void startPlayerConfig() throws Exception {
+        //TODO - this will start the player config windows
+    }
+
+
+    public void createGame(String difficulty,
+                           int numberOfPlayers,
+                           String map, Stage stage) {
+        mulegame = new Game(difficulty, numberOfPlayers, map, stage);
+    }
+
+
+    public static Game getMulegame() {
+        return mulegame;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
