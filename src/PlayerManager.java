aaab2c8f54144e287100a3c8b1f68f29d10adbad
Zurie Mai <pmai6@gatech.edu>
2015-09-21 12:14:46 -0400

    add buyProperty to playerManager

--- src/PlayerManager.java
+++ src/PlayerManager.java
@@ -2,34 +2,43 @@
  * Class to manage player objects
  */
 public class PlayerManager {
-
-
     /** Stub method
-     * Method to deduct the an amount of money a player spent
+     * Method to deduct the an amount of money a player spent on land
      * @param player
-     * @param item
-     * @param quantity
-     * @return true if able to do the method
+     * @param property
      */
-    public static void boughtsomething (Player player, String item, int quantity) {
-        if (item.equalsIgnoreCase("food")) {
-            player.getFood() = player.getFood() + (Store.getFood() * quantity);
-            player.getMoney() = player.getMoney() - (Store.getFood() * quantity);
-            Store.getFood() = Store.getFood() - (Store.getFood() * quantity);
-        } else if (item.equalsIgnoreCase("energy")) {
-            player.getEnergy() = player.getEnergy() + (Store.getEnergy() * quantity);
-            player.getMoney() = player.getMoney() - (Store.getEnergy() * quantity);
-            Store.getEnergy() = Store.getEnergy() - (Store.getEnergy() * quantity);
-        } else if (item.equalsIgnoreCase("smithore")) {
-            player.getSmithore() = player.getSmithore() + (Store.getSmithore() * quantity);
-            player.getMoney() = player.getMoney() - (Store.getSmithore() * quantity);
-            Store.getSmithore() = Store.getSmithore() - (Store.getSmithore() * quantity);
-        } else if (item.equals("crystite")) {
-            player.getCrystite() = player.getFood() + (Store.getCrystite() * quantity);
-            player.getMoney() = player.getMoney() - (Store.getCrystite() * quantity);
-            Store.getCrystite() = Store.getCrystite() - (Store.getCrystite() * quantity);
-        }
+    public static void buyProperty (Player player, Tile property) {
+        player.getMoney() = player.getMoney() - MapManager.costOfTile();
+        player.getTiles().add(property);
+
     }
+
+//    /** Stub method
+//     * Method to deduct the an amount of money a player spent
+//     * @param player
+//     * @param item
+//     * @param quantity
+//     * @return true if able to do the method
+//     */
+//    public static void boughtsomething (Player player, String item, int quantity) {
+//        if (item.equalsIgnoreCase("food")) {
+//            player.getFood() = player.getFood() + (Store.getFood() * quantity);
+//            player.getMoney() = player.getMoney() - (Store.getFood() * quantity);
+//            Store.getFood() = Store.getFood() - (Store.getFood() * quantity);
+//        } else if (item.equalsIgnoreCase("energy")) {
+//            player.getEnergy() = player.getEnergy() + (Store.getEnergy() * quantity);
+//            player.getMoney() = player.getMoney() - (Store.getEnergy() * quantity);
+//            Store.getEnergy() = Store.getEnergy() - (Store.getEnergy() * quantity);
+//        } else if (item.equalsIgnoreCase("smithore")) {
+//            player.getSmithore() = player.getSmithore() + (Store.getSmithore() * quantity);
+//            player.getMoney() = player.getMoney() - (Store.getSmithore() * quantity);
+//            Store.getSmithore() = Store.getSmithore() - (Store.getSmithore() * quantity);
+//        } else if (item.equals("crystite")) {
+//            player.getCrystite() = player.getFood() + (Store.getCrystite() * quantity);
+//            player.getMoney() = player.getMoney() - (Store.getCrystite() * quantity);
+//            Store.getCrystite() = Store.getCrystite() - (Store.getCrystite() * quantity);
+//        }
+//    }
 
 
     /**
