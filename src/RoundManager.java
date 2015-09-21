b0a162652589298e8c015d09f65290d0c2f65b1f
ryyan <ryyanj@gmail.com>
2015-09-21 10:39:45 -0400

    "made changeds to RoundManager and Round"

--- src/RoundManager.java
+++ src/RoundManager.java
@@ -2,10 +2,11 @@
  * This class need to keep track of rounds and notify the Game class or
  * manager that Land selection has finished
  */
-public class Round {
+public class RoundManager {
 
     public static void startRounds() {
 // no idea what this does
+        //should probably create turn objects here
     }
 
 
@@ -20,16 +21,15 @@ public class Round {
     }
 
 
-    /** other classes will call this to let the Round manager know that a
+    /** other classes will call this to let the RoundManager manager know that a
      * player has finished their turn.
      *  coming from MapManager.
      */
-    public static void playerFinishedTurn() { //TODO}
-
+    public static void playerFinishedTurn() { //TODO//
     }
 
 
-    /** other classes will call this to let the Round manager know that a
+    /** other classes will call this to let the RoundManager manager know that a
      * player has passed on turn
      *  coming Game Manager
      *  Will need to keep track of number of presses so can decide if land
@@ -38,9 +38,10 @@ public class Round {
      *  pressed in a round - if same as number of players in round then land
      *  selection phase is over.
      */
-    public static boolean playerPassedTurn() { //TODO}
+    public static boolean playerPassedTurn() {
         System.out.println("I finished my turn");
-return false;
+        Round.incrementNumPasses();
+        return false;
     }
 
 
@@ -48,6 +49,10 @@ return false;
      * over if all players have passed.
      */
     private static boolean isLandSelectionOver () { //TODO
-return false;
-}
+        if(Round.getNumPasses() == Game.getNumberOfPlayers()) {
+            return true;
+        }
+        else
+            return false;
+    }
 }
